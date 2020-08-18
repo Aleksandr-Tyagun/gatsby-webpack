f8763b2ec8695154c6368e7624a9a9ed7a0ae168
aleksandr-tyagun <alexander.tiagun@gmail.com>
2020-08-18 13:24:46 +0300

    axios and chalk were added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 import _ from "lodash"
+import axios from 'axios'
+import chalk from 'chalk'
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -8,8 +10,12 @@ import SEO from "../components/seo"
 
 const a = _.debounce(() => { console.log('hello') })
 a()
+axios(() => {})
 
-const IndexPage = () => (
+const IndexPage = () => {
+  console.log(chalk.blue('Hello world!'));
+  
+  return (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
@@ -21,6 +27,6 @@ const IndexPage = () => (
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
-)
+)}
 
 export default IndexPage
